4e175a8a671292f9ce72f9f5792a49003828f91d
wilitp <guilledeipola@gmail.com>
2024-08-15 16:38:22 -0300

    Zswap monitoring with unit conversion

--- polybar/dot-config/polybar/zswap.sh
+++ polybar/dot-config/polybar/zswap.sh
@@ -8,6 +8,6 @@ t=$(cat /proc/meminfo | grep zswapped -i | awk '{print $2}')
 r=$(echo $(cat /proc/meminfo | grep swaptotal -i | awk '{print $2}') - $(cat /proc/meminfo | grep swapfree -i | awk '{print $2}') - $t | bc)
 
 
-echo "$r $t"
+echo -n d=$(numfmt --to=iec-i --suffix=B --format="%9.2f" $(($r * 2**10))) z=$(numfmt --to=iec-i --suffix=B --format="%9.2f" $(($t * 2**10)))
 
 
